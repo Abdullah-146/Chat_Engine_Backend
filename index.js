4a36f0379158f0a9054259f20601be1e726a8e77
Abdullah-146 <abdullah.mumtaz65@gmail.com>
2023-04-18 01:09:35 +0500

    Abdullah : Comitted

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try{
+    const resp = await axios.put("https://api.chatengine.io/users/" , {
+        username: username,
+        secret: username,
+        first_name: username,
+    } , { headers : { "private-key" : process.env.API_KEY  }} )
+    return res.status(resp.status).json(resp.data);
+  }
+  catch(err){
+    return res.status(err.response.status).json(err.response.data);
+  }
+
+});
+// private key 3512c8a5-e3f7-42e6-8fc2-85cb050e05b8
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
